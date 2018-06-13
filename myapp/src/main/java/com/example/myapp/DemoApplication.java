1b91f31f05e7db66745d196dd8ff18ab63f85b03
kuyeii <39783769+kuyeii@users.noreply.github.com>
2018-06-13 15:44:24 -0500

    Add files via upload
    add entities to application

--- myapp/src/main/java/com/example/myapp/DemoApplication.java
+++ myapp/src/main/java/com/example/myapp/DemoApplication.java
@@ -1,21 +1,13 @@
-package demo;
+package com.telephone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Controller
 @SpringBootApplication
 public class DemoApplication {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Hello World!";
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 }
